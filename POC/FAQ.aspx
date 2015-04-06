a30396b7d67559d78f663bef0ff2e6836673b5bb
shobhit <shobhitkhandelwal@shobhit283401.Nagarro.local>
2015-04-06 17:19:30 +0530

    FAQ page added

---
+++ POC/FAQ.aspx
@@ -0,0 +1,22 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="FAQ.aspx.cs" Inherits="POC.FAQ" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+	<title></title>
+</head>
+<body>
+	<form id="form1" runat="server">
+		<div>
+			This content will be rendered on Chrome browser only.
+		</div>
+		<div>
+			This content will be rendered on Firefox browser only.
+		</div>
+		<div>
+			This content will be rendered on Opera mini browser only.
+		</div>
+	</form>
+</body>
+</html>
---
+++ POC/FAQ.aspx.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace POC
+{
+	public partial class FAQ : System.Web.UI.Page
+	{
+		protected void Page_Load(object sender, EventArgs e)
+		{
+			System.Web.HttpBrowserCapabilities browser = Request.Browser;
+			string s = "Browser Capabilities\n"
+				+ "Type = " + browser.Type + "\n"
+				+ "Name = " + browser.Browser + "\n"
+				+ "Version = " + browser.Version + "\n"
+				+ "Major Version = " + browser.MajorVersion + "\n"
+				+ "Minor Version = " + browser.MinorVersion + "\n"
+				+ "Platform = " + browser.Platform + "\n"
+				+ "Is Beta = " + browser.Beta + "\n"
+				+ "Is Crawler = " + browser.Crawler + "\n"
+				+ "Is AOL = " + browser.AOL + "\n"
+				+ "Is Win16 = " + browser.Win16 + "\n"
+				+ "Is Win32 = " + browser.Win32 + "\n"
+				+ "Supports Frames = " + browser.Frames + "\n"
+				+ "Supports Tables = " + browser.Tables + "\n"
+				+ "Supports Cookies = " + browser.Cookies + "\n"
+				+ "Supports VBScript = " + browser.VBScript + "\n"
+				+ "Supports JavaScript = " +
+					browser.EcmaScriptVersion.ToString() + "\n"
+				+ "Supports Java Applets = " + browser.JavaApplets + "\n"
+				+ "Supports ActiveX Controls = " + browser.ActiveXControls
+					  + "\n"
+				+ "Supports JavaScript Version = " +
+					browser["JavaScriptVersion"] + "\n";
+
+			Response.Write(s);
+
+		}
+	}
+}
