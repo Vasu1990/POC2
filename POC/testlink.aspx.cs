a00603e81262b622abb64ba1f8f7f168912ad09c
Vasu1990 <vasu.nagpal@nagarro.com>
2015-04-06 17:19:56 +0530

    link test url

--- POC/testlink.aspx.cs
+++ POC/testlink.aspx.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -11,7 +12,7 @@ namespace POC
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            YepMeLogo1.ImageUrl = "//" + ConfigurationManager.AppSettings["ShardingPathNetStorage"] + "/images/yep-me-logo_2.jpg";
         }
     }
 }
